58f99b2b500308390093a45631929c5d8b896c0e
user-juli <71297883+user-juli@users.noreply.github.com>
2023-10-26 00:26:09 -0500

    create a test api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 //import { PORT, mongoDBURL } from "./config.js";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -13,11 +14,13 @@ const mongoDBURL = process.env.mongoDBURL;
 mongoose
   .connect(mongoDBURL)
   .then(() => {
-    console.log('App connected to database');
+    console.log("App connected to database");
     app.listen(PORT, () => {
       console.log(`App is listening to port: ${PORT}`);
     });
   })
   .catch((error) => {
     console.log(error);
-  })
+  });
+
+app.use('/api/user', userRouter);
